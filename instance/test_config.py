e46c481963731e7b930b0f522bfcafd16e2fea44
Ernesto <sereernest@gmail.com>
2025-04-18 01:34:02 +0300

    Update test_config.py

--- instance/test_config.py
+++ instance/test_config.py
@@ -1,30 +1,8 @@
 import os
-
 from dotenv import load_dotenv
-
+from .config import Config
 load_dotenv("instance/env/.env")
 
-
-class TestConfig:
-    # Используйте SQLite в памяти для тестирования
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Выставим секретный ключ для тестов
-    SECRET_KEY = "test_secret_key"
-
-    # Отключим Redis для тестирования
-    REDIS_URL = None
-
-    # Настройте e-mail сервер для тестирования
-    MAIL_SERVER = "smtp.test.com"  # Используйте заглушку вместо настоящего сервиса
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv("TEST_MAIL_USERNAME", "")
-    MAIL_PASSWORD = os.getenv("TEST_MAIL_PASSWORD", "")
-
-    OAUTH2_PROVIDERS = {}
-
-
-def load_test_config(app):
-    app.config.from_object(TestConfig)
+class TestConfig(Config):
+    PORT = 5000
+    HOST = '127.0.0.1'
